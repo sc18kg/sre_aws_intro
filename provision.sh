031240c12db541ce0a315bb6967fc934a9b23d37
Kieron Gordon <sc18kg@gmail.com>
2021-08-31 15:55:43 +0100

    Changes to provision files

--- provision.sh
+++ provision.sh
@@ -1,23 +1,22 @@
-#!/bin/bash
+!#/bin/bash
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y 
 sudo apt-get install nodejs -y
-sudo apt-get install npm -y
 sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 #inside app folder
-cd ../ubuntu/app
+cd /home/ubuntu/app
 sudo npm install pm2 -g
 sudo npm install
 sudo rm /etc/nginx/sites-available/default
 sudo ln -s /home/ubuntu/new_config/default /etc/nginx/sites-available/default
 sudo nginx -t
-sudo systemctl restart ngninx
-echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
+sudo systemctl restart nginx
+echo "export DB_HOST=192.168.10.150:27017/posts/" >> /etc/environment
 source /etc/environment
 
 
